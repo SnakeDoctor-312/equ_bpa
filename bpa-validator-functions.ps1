bb1015aebfc7eb7eed91a4ad0b6770fe6c153a42
mdeliberto <56379253+mdeliberto@users.noreply.github.com>
2019-12-24 06:45:01 -0600

    Refactoring
    Massive Refactoring effort
    Code is much better now

--- bpa-validator-functions.ps1
+++ bpa-validator-functions.ps1
@@ -14,22 +14,41 @@ function isNotEmptyNull {
     [OutputType([bool])]
     [Parameter(Position=0, Mandatory=$True,ParameterSetName="Array")]
     [System.Object[]]$array,
+    #[Parameter(Position=0, Mandatory=$True,ParameterSetName="Double")]
+    #[double]$double,
     [Parameter(Position=0, Mandatory=$True,ParameterSetName="HashTable")]
     [System.Collections.Hashtable]$table,
     [Parameter(Position=0, Mandatory=$True,ParameterSetName="OrderedDictionary")]
-    [System.Collections.Specialized.OrderedDictionary]$ordereddictionary,
-    [Parameter(Position=0, Mandatory=$True,ParameterSetName="String")]
-    [string]$string)
+    [System.Collections.Specialized.OrderedDictionary]$ordereddictionary)
     switch ($PsCmdlet.ParameterSetName)
     {
         "Array" {return (($array -ne $null) -and ($array.count -ne 0))}
         "HashTable" {return -not ($table -ne $null) -and ($table.count -ne 0)}
+        #"Double" {return -not (dDouble -ne $null)}
         "OrderedDictionary" {return -not ($OrderedDictionary -ne $null) -and ($OrderedDictionary.count -ne 0)}
-        "String" {return -not ($string -ne $null) -and ($string.length -ne 0)}
     }
 
 }
 
+function isEmptyNullWhiteSpace {
+    [CmdletBinding()]
+    param(
+    [OutputType([bool])]
+    [Parameter(Position=0, Mandatory=$True)]
+    [string]$in)
+    return !([String]::IsNullOrEmpty($in)  -or [String]::IsNullOrWhiteSpace($in))
+}
+
+function isNotEmptyNullWhiteSpace {
+    [CmdletBinding()]
+    param(
+    [OutputType([bool])]
+    [Parameter(Position=0, Mandatory=$True)]
+    [string]$in)
+    return (-not (isEmptyNullWhiteSpace $in))
+}
+
+<##
 function isEmptyNull {
     [CmdletBinding()]
     param(
@@ -37,4 +56,4 @@ function isEmptyNull {
     [Parameter(Position=0, Mandatory=$True)]
     $arg)
     return $(-not $(isNotEmptyNull $arg))
-}
+}##>
--- cwm-bpa-manager.ps1
+++ cwm-bpa-manager.ps1
@@ -1,140 +1,18 @@
 ﻿. "$PSScriptRoot\cwm-server-rest.ps1"
+. "$PSScriptRoot\cwm-company.ps1"
 . "$PSScriptRoot\bpa-date-functions.ps1"
 . "$PSScriptRoot\bpa-validator-functions.ps1"
+. "$PSScriptRoot\bpa-task.ps1"
 
-$TaskFolder = "$PSScriptRoot\Tasks"
+$TaskFolder = "$PSScriptRoot\Tasks2"
 $CompanyIDFile = "$PSScriptRoot\CompanyIDs.csv"
 
-#$VerbosePreference = "continue"
+$VerbosePreference = "continue"
 #$VerbosePreference = "SilentlyContinue"
-
-class Company {
-    [string]$abbreviation
-    [uint32]$id
-    [string]$identifier
-    [string]$name
-    [string]$path
-
-    Company([string] $abbreviation, [uint32]$id) {
-        $this.abbreviation = $abbreviation
-        $this.id = $id
-    }
-   
-} 
-
-function Load-CompanyData {
-    [CmdletBinding()]
-    [OutputType([Company[]])]
-    Param (
-        [validateScript({ isNotEmptyFile($_) })]
-        [string] $companyfile,
-        [validateScript({ Test-path -Path $_ -PathType Container })]
-        [string] $path)
-    
-    [Company[]]$retVal  =@()
-    
-    $companyIDs = import-csv -Path $companyfile
-    
-    if (isEmptyNull($companyIDs)) {
-        write-error "$companyfile returned a null or empty hashtable"
-    }
-    
-    Write-Verbose "Imported from file: $companyfile"
-    $CompanyIDs | FT | Out-String | Write-Verbose
-    Write-Verbose "Located $($CompanyIDs.Count) rows"
-        
-    foreach ($CompanyID in $companyIDs) {
-        [Company]$NewCompany = [Company]::new($CompanyID.Company, $CompanyID.ID)
-        
-        $CompanyJSON = $server.GetCompany($NewCompany.id)
-        
-        $NewCompany.identifier = $CompanyJSON.identifier
-        $NewCompany.name = $CompanyJSON.name
-        
-        $ABBR =  $NewCompany.abbreviation
-        $CompanyFile = $path + "\"+$ABBR + ".csv"
-        if (isNotEmptyFile $companyFile) {
-            $NewCompany.path = $CompanyFile
-        }
-        $retVal += $NewCompany
-    }
-
-    return $retVal;
-
-}
-
-
-
-enum TaskFrequency {
-        Weekly =  416;
-        Monthly = 417;
-        Quarterly =  418;
-        SemiAnnual = 419;
-        Annual =    420;
-        Daily   =   421;
-}
-
-enum TaskCatergory {
-        System      = 563
-        Application = 564
-        Network     = 565
-        Desktop     = 566
-        Security    = 567
-        Cloud       = 568
-        Storage     = 569
-}
-
-
-class TaskSet {
-    [string]$clientID
-
-    TaskSet([string] $ClientName, [Task[]]$taskList) {
-        $this.clientID = $ClientName;
-        $this.tasks = $taskList;
-    }
-
-}
-
-class Task {
-    [string]$Summary
-    [double]$Budget
-    [TaskFrequency]$Type
-    [TaskCatergory]$subtype
-    [string]$owner
-    [string]$DueDate
-    [string]$Description
-
-    Task([string]$Summary, [String]$Frequency, [double]$Budget, [string]$subtype,[string]$owner,[string]$InitialDescription)     {
-        [datetime]$TaskDueDate = Get-date -Year 2019 -Month 12 -Date 28 -Hour 17 -Minute 30 -Second 0;
-        $this.Summary = $Summary;
-        $this.type = [TaskFrequency]$Frequency
-        $this.Budget = $budget;
-        $this.subtype = [TaskCatergory]$subtype
-        $this.owner = $owner;
-
-        switch($this.Type) {
-            ([TaskFrequency]::Weekly)
-                {$TaskDueDate = Get-date -Year 2019 -Month 12 -Day 20 -Hour 17 -Minute 30 -Second 0}
-            ([TaskFrequency]::Monthly)
-                 {$TaskDueDate = Get-date -Year 2019 -Month 11 -Day 30 -Hour 17 -Minute 30 -Second 0}
-            ([TaskFrequency]::Quarterly)
-                 {$TaskDueDate = Get-date -Year 2019 -Month 12 -Day 28 -Hour 17 -Minute 30 -Second 0}
-            ([TaskFrequency]::SemiAnnual)
-                 {$TaskDueDate = Get-date -Year 2019 -Month 12 -Day 28 -Hour 17 -Minute 30 -Second 0}
-            ([TaskFrequency]::Annual)
-                 {$TaskDueDate = Get-date -Year 2019 -Month 12 -Day 28 -Hour 17 -Minute 30 -Second 0}
-            default
-                {$TaskDueDate = Get-date -Year 2019 -Month 12 -Day 28 -Hour 17 -Minute 30 -Second 0}
-        }
-        $this.DueDate = $($TaskDueDate).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
-        #write-host $TaskDuedate $frequency $this.DueDate
-        #2019-09-14T17:59:51Z
-    }
-}
 
 
 $Server = [CWServer]::new("equilibrium", "eqwf.equilibriuminc.com", "MLDBHBvh5LNLyvuK", "QMcVdAojN8p6EA9J")
-$server.connect()
+$null = $server.connect()
 
 
 $Companies = Load-CompanyData $CompanyIDFile $TaskFolder
@@ -142,66 +20,50 @@ $Companies = Load-CompanyData $CompanyIDFile $TaskFolder
 $Companies | ft
 
 foreach ($company in $Companies) {
-    $tasks = import-csv $company.Path
+    $company
+    write-host $company.path
+    $tasks = Load-ClientTasks $company.path
     foreach ($task in $tasks) {
-        if ($task -ne "") {
-            if (isNewWeek) {
-                #close all weeklies
-                #Create Weelkies
+        #if (isNewWeek) {
+            #Create Weelkies
+            if ($Task.Frequency -eq "Weekly") {
+                $company.abbreviation
+                $task.Summary
+                #$newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
+                #$Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
             }
-            if (isNewMonth) {
-                #close all weeklies
-                #Create Weelkies
+        #}
+        #if (isNewMonth) {
+            if ($Task.Frequency -eq "Monthly") {
+                $company.abbreviation
+                $task.Summary
+                #$newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
+                #$Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
             }
-            if (isNewQuarter) {
-                #close all weeklies
-                #Create Weelkies
+        #}
+        #if (isNewQuarter) {
+            if ($Task.Frequency -eq "Quarterly") {
+                $company.abbreviation
+                $task.Summary
+                #$newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
+                #$Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
             }
-            if (isNewHalfYear) {
-                #close all weeklies
-                #Create Weelkies
+        #}
+        #if (isNewHalfYear) {
+            if ($Task.Frequency -eq "SemiAnnual") {
+                $company.abbreviation
+                $task.Summary
+                #$newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
+                #$Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
             }
-            if (isNewYear) {
-                #close all weeklies
-                #Create Weelkies
+        #}
+        #if (isNewYear) {
+                if ($Task.Frequency -eq "Annual") {
+                $company.abbreviation
+                $task.Summary
+                #$newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
+                #$Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
             }
-        } else {
-            ###
-        }
+        #}
     }
 }
-    <##foreach ($Task in $tasks) {
-        if ($task.Task -ne "") {
-            if ($Task.Freq -eq "Weekly") {
-                $newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
-                $Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
-            }
-           # if ($Task.Freq -eq "Monthly") {
-            #    $newTask = [Task]::new($task.Task, $Task.Freq, $Task.Budget, "System", $Task.Engineer, "None")
-             #   $Server.CreateTicket($newTask.Summary, "None at this time", $company.id, $newTask.owner, 71, $newTask.Type, $newTask.subtype, 1022, $newtask.DueDate, $newTask.budget)
-            #}
-            
-        }
-    }
-}
-
-    
-
-
-
-
-#$a = $BPAReport_Type.Monthly
-<#
-
-
-$Server = [CWServer]::new("equilibrium", "eqwf.equilibriuminc.com", "MLDBHBvh5LNLyvuK", "QMcVdAojN8p6EA9J")
-
-
-
-
-foreach ($file in $files) {
-    
-}
-
-
-#>
