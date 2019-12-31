9b60415458e1ce6d14c9a1358b2ab5fbaf20d989
mdeliberto <56379253+mdeliberto@users.noreply.github.com>
2019-12-31 07:29:28 -0600

    Forced Reaffirm

---
+++ bpa-task.ps1
@@ -0,0 +1,104 @@
+﻿. "$PSScriptRoot\bpa-date-functions.ps1"
+
+enum TaskFrequency {
+        Weekly =  416;
+        Monthly = 417;
+        Quarterly =  418;
+        SemiAnnual = 419;
+        Annual =    420;
+        Daily   =   421;
+}
+
+enum TaskCategory {
+        System      = 563
+        Application = 564
+        Network     = 565
+        Desktop     = 566
+        Security    = 567
+        Cloud       = 568
+        Storage     = 569
+}
+
+class Task {
+    [string]$Summary
+    [double]$Budget
+    [TaskFrequency]$Frequency
+    [TaskCategory]$Category
+    [string]$owner
+    [string]$DueDate
+    [string]$Description
+
+    Task([string]$Summary, [string]$Frequency, [double]$Budget, [string]$owner,[string]$InitialDescription, [string]$Category)     {
+        [datetime]$TaskDueDate = Get-Date -Hour 17 -Minute 30 -Second 0;
+        
+        [bool] $parameterIssue = $false
+        
+        $this.Frequency = [TaskFrequency]$Frequency
+        $this.Category = [TaskCategory]$Category
+
+        $castBudget = [double]::Parse($Budget)
+        if ($castBudget -lt 0) {
+            Write-verbose "Budget is $castBudget. Changing to 0"
+            $this.Budget = 0
+        } else {
+            $this.Budget = $castBudget
+        }
+
+        switch($this.Type) {
+            ([TaskFrequency]::Weekly)
+                {$TaskDueDate = get-WeeklyDueDate}
+            ([TaskFrequency]::Monthly)
+                 {$TaskDueDate = get-MonthlyDueDate}
+            ([TaskFrequency]::Quarterly)
+                 {$TaskDueDate = get-QuarterlyDueDate}
+            ([TaskFrequency]::SemiAnnual)
+                 {$TaskDueDate = get-SemiAnnualDueDate}
+            ([TaskFrequency]::Annual)
+                 {$TaskDueDate = get-AnnualDueDate}
+            default
+                {$TaskDueDate = Get-date -Hour 17 -Minute 30 -Second 0}
+        }
+        $this.DueDate = $($TaskDueDate).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+    }
+}
+
+function Load-ClientTasks {
+    [CmdletBinding()]
+    [OutputType([Task[]])]
+    Param (
+        [validateScript({ isNotEmptyFile($_) })]
+        [string] $clientTasks)
+    
+    [Task[]]$retVal  =@()
+    
+    $taskImport = import-csv -Path $clientTasks
+    
+    if (-not (isNotEmptyNull($taskImport))) {
+        write-error "$clientTasks returned a null or empty hashtable"
+    }
+    
+    Write-Verbose "Imported from file: $clientTasks"
+    $taskImport | FT | Out-String | Write-Verbose
+    Write-Verbose "Located $($taskImport.Count) rows"
+    
+    foreach ($taskRow in $taskImport) {
+        if ([string]::IsNullOrEmpty($taskRow.Summary)     -or [string]::IsNullOrWhiteSpace($taskRow.Summary) -or `
+            [string]::IsNullOrEmpty($taskRow.Budget)      -or [string]::IsNullOrWhiteSpace($taskRow.Budget) -or `
+            [string]::IsNullOrEmpty($taskRow.Category)   -or [string]::IsNullOrWhiteSpace($taskRow.Category) -or `
+            [string]::IsNullOrEmpty($taskRow.Description) -or [string]::IsNullOrWhiteSpace($taskRow.Description) -or `
+            [string]::IsNullOrEmpty($taskRow.Freq)        -or [string]::IsNullOrWhiteSpace($taskRow.Freq) -or `
+            [string]::IsNullOrEmpty($taskRow.Engineer)    -or [string]::IsNullOrWhiteSpace($taskRow.Engineer)) {
+            write-warning "Found an empty, null or white space string in file: $clientTasks `n will gracefully drop:`n$taskrow.`n Error: Your inputfile has empty/bad data."
+            break;    
+        } else {
+           [Task]$NewTask = [Task]::new($taskRow.Summary, [TaskFrequency]$taskRow.Freq, $taskRow.Budget, $taskRow.Engineer, $taskRow.Description, [TaskCategory]$taskRow.Category)
+                  
+            if ($newTask -ne $null) {
+                $retVal += $newTask
+            }
+        }
+    }
+
+    return $retVal;
+
+}
---
+++ cwm-company.ps1
@@ -0,0 +1,61 @@
+﻿class Company {
+    [string]$abbreviation
+    [uint32]$id
+    [string]$identifier
+    [string]$name
+    [string]$path
+
+    Company([string] $abbreviation, [uint32]$id) {
+        $this.abbreviation = $abbreviation
+        $this.id = $id
+    }
+   
+} 
+
+function Load-CompanyData {
+    [CmdletBinding()]
+    [OutputType([Company[]])]
+    Param (
+        [validateScript({ isNotEmptyFile($_) })]
+        [string] $companyfile,
+        [validateScript({ Test-path -Path $_ -PathType Container })]
+        [string] $path)
+    
+    [Company[]]$retVal  =@()
+    
+    $companyIDs = import-csv -Path $companyfile
+
+    if (-not (isNotEmptyNull $companyIDs)) {
+        write-error "$companyfile returned a null or empty hashtable"
+        Write-Error "This means the file is likely empty"
+    }
+    
+    Write-Verbose "Imported from file: $companyfile"
+    $CompanyIDs | FT | Out-String | Write-Verbose
+    Write-Verbose "Located $($CompanyIDs.Count) rows"
+        
+    foreach ($CompanyID in $companyIDs) {
+        [Company]$NewCompany = [Company]::new($CompanyID.Company, $CompanyID.ID)
+
+        $CompanyJSON = $server.GetCompany($NewCompany.id)
+
+        if ($CompanyJSON.identifier -notlike "$($CompanyID.Company)*") {
+            #Write-Warning -WarningAction Continue "Error while processing $companyfile`n While validating the following line from the file: $($CompanyID.Company), $($CompanyID.ID)`n Error: $($CompanyJSON.identifier) mismtach with $($NewCompany.abbreviation)" 
+        }
+                
+        $NewCompany.identifier = $CompanyJSON.identifier
+        $NewCompany.name = $CompanyJSON.name
+        
+        $ABBR =  $NewCompany.abbreviation
+        $CompanyFile = $path + "\"+$ABBR + ".csv"
+        if (isNotEmptyFile $companyFile) {
+            $NewCompany.path = $CompanyFile
+            (get-content $companyfile) -replace ",,,,,,","" |  Out-File $companyfile
+            (get-content $companyfile) | ? {$_.trim() -ne "" } | set-content $companyfile
+        }
+        $retVal += $NewCompany
+    }
+
+    return $retVal;
+
+}
